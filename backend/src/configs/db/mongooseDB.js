4eb27b957792f8f4c748b01800528b4b0e425d22
Bikram Rai <bikramdevlopment@gmail.com>
2025-01-24 01:26:18 +0545

    fixes on db

--- backend/src/configs/db/mongooseDB.js
+++ backend/src/configs/db/mongooseDB.js
@@ -2,14 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const dbURI = `${process.env.MONGOOSE_URI}/${process.env.MONGOOSE_DB_NAME}?retryWrites=true&w=majority`;
-
-    console.log("Connecting to MongoDB with URI:", dbURI);
-
-    const connectedDB = await mongoose.connect(dbURI); // Connect to MongoDB
+    const connectedDB = await mongoose.connect(process.env.MONGOOSE_URI);
     console.log(`Connected to MongoDB host: ${connectedDB.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection failed: ${error.message}`);
-    process.exit(1); // Exit on failure
+    process.exit(1);
   }
 };
